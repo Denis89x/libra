8b946470dda24cd8e38d9f3c830a940b4a76e649
Lebenkov Denis <lebenkov.denis@mail.ru>
2024-03-29 20:27:32 +0300

    project initalized

---
+++ src/main/java/dev/lebenkov/libra/LibraExchangeApplication.java
@@ -0,0 +1,13 @@
+package dev.lebenkov.libra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lebenkov/libra/LibraExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lebenkov.libra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
