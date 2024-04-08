d7fd72c90c79e06bada699d7cc4467b0408407c8
Lebenkov Denis <lebenkov.denis@mail.ru>
2024-04-08 13:24:11 +0300

    Added registration service test

---
+++ src/test/java/dev/lebenkov/libra/api/service/RegistrationServiceImplTest.java
@@ -0,0 +1,81 @@
+package dev.lebenkov.libra.api.service;
+
+import dev.lebenkov.libra.api.security.JwtUtil;
+import dev.lebenkov.libra.storage.dto.AuthResponse;
+import dev.lebenkov.libra.storage.dto.UserRegistrationRequest;
+import dev.lebenkov.libra.storage.enums.TokenType;
+import dev.lebenkov.libra.storage.model.Token;
+import dev.lebenkov.libra.storage.model.User;
+import dev.lebenkov.libra.storage.repository.TokenRepository;
+import dev.lebenkov.libra.storage.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class RegistrationServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private TokenRepository tokenRepository;
+
+    @Mock
+    private JwtUtil jwtUtil;
+
+    @Mock
+    private TokenService tokenService;
+
+    @Mock
+    private AccountDetailsService accountDetailsService;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private RegistrationServiceImpl registrationService;
+
+    @Test
+    void RegistrationService_Register_ReturnsAuthResponse() {
+        // Arrange
+        User user = User.builder()
+                .username("testusername")
+                .email("testEmail@mail.ru")
+                .password("testPassword")
+                .role("ROLE_USER")
+                .registrationDate(LocalDate.now())
+                .build();
+
+        UserRegistrationRequest userRegistrationRequest = UserRegistrationRequest.builder()
+                .username("testusername")
+                .password("testPassword")
+                .email("testEmail@mail.ru")
+                .build();
+
+        UserDetails userDetails = mock(UserDetails.class);
+
+        // Act
+        when(userRepository.save(any(User.class))).thenReturn(user);
+        when(accountDetailsService.loadUserByUsername(user.getUsername())).thenReturn(userDetails);
+        when(jwtUtil.generateToken(userDetails)).thenReturn("accessToken");
+        when(jwtUtil.generateRefreshToken(userDetails)).thenReturn("refreshToken");
+
+        AuthResponse response = registrationService.register(userRegistrationRequest);
+
+        // Assert
+        assertNotNull(response);
+        assertEquals("accessToken", response.getAccessToken());
+        assertEquals("refreshToken", response.getRefreshToken());
+    }
+}
